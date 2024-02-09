c535dcba98aa14bc09d004c1bf6d4ad59548f651
thejayfrid <jasonfridley1@gmail.com>
2024-02-09 09:13:39 -0500

    OB

--- Prototype 1(Truck)/Assets/script/PlayerController.cs
+++ Prototype 1(Truck)/Assets/script/PlayerController.cs
@@ -14,6 +14,6 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
