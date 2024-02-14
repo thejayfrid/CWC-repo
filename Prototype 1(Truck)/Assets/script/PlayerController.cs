0b79b764ac2bf7a094c0d1b975048dcd63259f8a
thejayfrid <jasonfridley1@gmail.com>
2024-02-14 10:43:29 -0500

    dak

--- Prototype 1(Truck)/Assets/script/PlayerController.cs
+++ Prototype 1(Truck)/Assets/script/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 20;
-    
+    public float turnspeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
